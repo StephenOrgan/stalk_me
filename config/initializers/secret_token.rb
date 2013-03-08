4fffefc1646c616a0f1ab836638b87bcc0ebed18
Steve Organ <steve@influitive.com>
2013-03-07 21:12:54 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StalkMe::Application.config.secret_token = '8c34ad50bae8a9a9c604636e5eccdbbc9f38bf6ea3144a085e308fc87284576e945fe1d2cda98149b71f9312c05978e12bc0ffdac26afacf9e674882a7956fac'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+StalkMe::Application.config.session_store :cookie_store, key: '_stalk_me_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# StalkMe::Application.config.session_store :active_record_store
